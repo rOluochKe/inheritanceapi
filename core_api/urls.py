6c8f3c17be0108df5076653e7d26356bcca55884
Raymond Oluoch <rasholuoch@gmail.com>
2024-02-21 00:06:36 +0300

    generated static files

--- core_api/urls.py
+++ core_api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api/v1/users/', include('user.urls')),
     path('api/v1/categories/', include('category.urls')),
     path('api/v1/items/', include('item.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT)
